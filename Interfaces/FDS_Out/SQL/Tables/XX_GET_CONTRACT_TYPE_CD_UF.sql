b7548c1c0d4ae60f131c74967771a78eb5b042ba
George Alvarez <george.alvarez@us.ibm.com>
2022-05-23 19:46:15 -0400

    FIN190 changes
    Files to bring us into compliance with FIN190

---
+++ Interfaces/FDS_Out/SQL/Tables/XX_GET_CONTRACT_TYPE_CD_UF.sql
@@ -0,0 +1,49 @@
+USE [IMAPSStg]
+GO
+
+/****** Object:  UserDefinedFunction [dbo].[XX_GET_CONTRACT_TYPE_CD_UF]    Script Date: 5/4/2022 3:19:11 PM ******/
+DROP FUNCTION [dbo].[XX_GET_CONTRACT_TYPE_CD_UF]
+GO
+
+/****** Object:  UserDefinedFunction [dbo].[XX_GET_CONTRACT_TYPE_CD_UF]    Script Date: 5/4/2022 3:19:12 PM ******/
+SET ANSI_NULLS OFF
+GO
+
+SET QUOTED_IDENTIFIER OFF
+GO
+
+CREATE FUNCTION [dbo].[XX_GET_CONTRACT_TYPE_CD_UF](@in_proj_id varchar(30))  
+RETURNS varchar(2) AS  
+BEGIN
+/************************************************************************************************  
+Name:       	[XX_GET_CONTRACT_TYPE_CD_UF]
+Author:     	GA
+Created:    	05/2022 
+
+CR 13721 - Get SERVICES CONTRACT TYPE FOR 999 FILE FROM PROJ_ID
+RETURN VALUE ?? MEANS WE'VE ADDED A NEW ONE AND NEED TO CHANGE THIS CODE
+
+************************************************************************************************/
+
+DECLARE @BILL_FORMULA varchar(2)
+
+SELECT @BILL_FORMULA = 
+	CASE 
+	 WHEN LEFT(S_BILL_FORMULA_CD,1) = 'C' THEN 'CP'  -- COST PLUS
+	 WHEN LEFT(S_BILL_FORMULA_CD,3) = 'LLR' THEN 'HR'  -- HOURLY
+	 WHEN LEFT(S_BILL_FORMULA_CD,2) = 'RS' THEN 'HR'  -- HOURLY
+	 WHEN S_BILL_FORMULA_CD = 'NONE' THEN 'FP'  -- FIXED PRICE
+	 WHEN S_BILL_FORMULA_CD = 'UNIT' THEN 'UP'  -- UNIT PRICE
+	 ELSE '  '
+	END
+	FROM imaps.dbo.XX_STAGE_PROJ_BILL_INFO  
+	WHERE INVC_PROJ_ID = @in_proj_id
+
+RETURN isnull(@BILL_FORMULA,'  ')
+
+END
+
+
+GO
+
+
